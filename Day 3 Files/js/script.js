4c401db5453951954d0ca30f0d6d9b1a5d97967e
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-11 18:22:24 -0400

    Setup First Files
    Created HTML and JS file using web storm

---
+++ Day 3 Files/js/script.js
@@ -0,0 +1,13 @@
+/*
+Brian Cobb
+WPF 1407
+7-11-2014
+Day 3 First File
+ */
+
+
+/*
+    this is example of multi-line comment anything between
+    the star and slash will not be affected.
+ */
+alert("we are in WPF Class and we are programing");
