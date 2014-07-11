85eed5a8206f79ea17c8766e20b1039db63aa66d
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-11 18:32:59 -0400

    Covered comments, a lets and console
    Covered comments, a lets and console

--- Day 3 Files/js/script.js
+++ Day 3 Files/js/script.js
@@ -10,4 +10,16 @@ Day 3 First File
     this is example of multi-line comment anything between
     the star and slash will not be affected.
  */
+
+
+// Single line comment
+
+
+// Example comment goes here
+
+// alert() - creates pop up box
+
 alert("we are in WPF Class and we are programing");
+
+// console.log() - coders use to troubleshoot code
+console.log("this is a console statement")
