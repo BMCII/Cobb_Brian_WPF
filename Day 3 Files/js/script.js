e8dbf93b6712807d2ae8fc6d2336db3b74373dd7
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-11 19:35:53 -0400

    creating variables
    storage boxes

--- Day 3 Files/js/script.js
+++ Day 3 Files/js/script.js
@@ -5,21 +5,52 @@ WPF 1407
 Day 3 First File
  */
 
-
 /*
     this is example of multi-line comment anything between
     the star and slash will not be affected.
  */
 
-
 // Single line comment
-
 
 // Example comment goes here
 
 // alert() - creates pop up box
-
 alert("we are in WPF Class and we are programing");
 
 // console.log() - coders use to troubleshoot code
-console.log("this is a console statement")
+console.log("this is a console statement");
+
+
+//Declare a variable
+//use keyword var
+var whatever1;  //Declaration of a variable
+whatever1 = 42;  //Definition of a variable - giving it a value
+
+// storageContainer = value; - define a variable
+
+//Setup and define the variable in one step
+var a = 2;  //Declaring and defining in one step
+
+//check the value of the a variable
+console.log(a);
+
+//how do we add in JS?   +
+
+a = a+3;
+
+console.log(a);
+
+
+//Declare a new variable
+var b;
+
+//Define b using variable a
+
+b = a+3;
+
+console.log (b);
+
+
+
+
+
