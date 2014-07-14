b2cbb22bd11fce06f0b6f9fc1172a24e3de673a8
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-14 18:12:22 -0400

    completed arrays
    variables of variables

--- Day 4 Files/js/arrays.js
+++ Day 4 Files/js/arrays.js
@@ -8,4 +8,37 @@
 
 //alert("testing 1,2,3");
 
+//create basic array
+var avengersNames = ["Iron Man","Hulk","Thor","Captain America"];
 
+//print assay
+console.log(avengersNames);
+
+//access a specific item in array
+
+console.log(avengersNames[3]);
+
+//set specific item
+
+avengersNames[0] = "hawkeye";
+console.log(avengersNames);
+avengersNames[4] = "Antman";
+console.log(avengersNames);
+console.log(avengersNames.length);
+//counts items in array
+
+//last index number is one less than length
+
+//Picking Oranges
+
+var orangeBins = [234,567,883];
+
+//how many oranges picked
+
+var totalOranges = orangeBins[0] + orangeBins[1] +orangeBins[2];
+console.log("the total number of oranges picked is " + totalOranges);
+var average = totalOranges / orangeBins.length;
+
+//to round number
+//.tofixed()
+console.log("the average number of oranges is " +average.toFixed(2));
