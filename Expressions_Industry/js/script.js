72f3523173f2a3168246cf05fd14f60c9b9908b7
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-14 23:28:26 -0400

    array
    array to clean up

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -16,14 +16,18 @@ var income = prompt("what is your income per pay period?");
 var expenses = prompt("what are your total expenses per pay period?");
 var percentSavings = prompt("what percent of your disposable income would you like to save");
 
-//need parseInt in case user inputs more than numbers
+//need parseInt to change text string to number
 
 var savingsGoalNumber = parseInt(savingsGoal);
 var incomeNumber = parseInt(income);
 var expensesNumber = parseInt(expenses);
 var percentSavingsNumber = parseInt(percentSavings);
 
+//made an array to clean up code
+
+var variables = [savingsGoalNumber, incomeNumber, expensesNumber, percentSavingsNumber];
+
 //savingsGoal / (( income - expenses ) * ( percentSavings / 100 ))
 
-var time = savingsGoalNumber / ((incomeNumber - expensesNumber) * (percentSavingsNumber / 100));
+var time = variables[0] / ((variables[1] - variables[2]) * (variables[3] / 100));
 var answer = alert("It will take " + time + " pay periods to meet your goal");
