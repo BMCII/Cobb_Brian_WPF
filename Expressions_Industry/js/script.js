65fb0e94e7c11823e4124de162040ce4cd530f85
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-15 18:15:56 -0400

    formula complete
    it works

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -11,14 +11,26 @@
 
 var baseCost = (250);
 var numberOfPages = prompt("How many pages would you like?");
-var addOns = prompt("how many add-ons would you like\n shopping cart, SEO, maintenance, Logo");
+var addOns = prompt("how many add-ons would you like\n shopping cart,\n SEO, \n maintenance, \n Logo");
 var type = prompt("ecomerce, basic or CMS?");
 
 //how to give a value to var type?
 
-var totalCost = (baseCost + (numberOfPages *  50) + (addOns * 50));
+
+
+var baseCostNumber = parseInt(baseCost);
+var numberOfPagesNumber = parseInt(numberOfPages);
+var addOnsNumber = parseInt(addOns);
+
+var totalCost = (baseCostNumber + (numberOfPagesNumber *  50) + (addOnsNumber * 50));
 console.log(totalCost);
 
+console.log(baseCostNumber);
+console.log(numberOfPagesNumber);
+console.log(addOnsNumber);
+
 //working so far
+
+var answer = alert("you would like a(an) " + type + " website for $" + totalCost);
 
 
