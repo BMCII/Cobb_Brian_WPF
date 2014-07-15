4f14142a7cef6b701896b824bccfa5bca2368075
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-14 23:22:17 -0400

    parseInt
    added parseInt

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,7 +7,7 @@
 
 //alert("testing 1,2,3");
 
-//how much of disposable income goes to savings
+//how to calculate time to save money
 //income - expenses - desired percent to save
 
 alert("savings calculator");
@@ -16,6 +16,14 @@ var income = prompt("what is your income per pay period?");
 var expenses = prompt("what are your total expenses per pay period?");
 var percentSavings = prompt("what percent of your disposable income would you like to save");
 
+//need parseInt in case user inputs more than numbers
+
+var savingsGoalNumber = parseInt(savingsGoal);
+var incomeNumber = parseInt(income);
+var expensesNumber = parseInt(expenses);
+var percentSavingsNumber = parseInt(percentSavings);
+
 //savingsGoal / (( income - expenses ) * ( percentSavings / 100 ))
-var time = savingsGoal / ((income - expenses) * (percentSavings / 100));
+
+var time = savingsGoalNumber / ((incomeNumber - expensesNumber) * (percentSavingsNumber / 100));
 var answer = alert("It will take " + time + " pay periods to meet your goal");
