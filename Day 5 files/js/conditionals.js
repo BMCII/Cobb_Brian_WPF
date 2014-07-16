e815cd520ab8817ae5479189837b067fb859caba
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-16 18:53:38 -0400

    basic conditional
    basic conditional

--- Day 5 files/js/conditionals.js
+++ Day 5 files/js/conditionals.js
@@ -10,7 +10,7 @@
 //basic conditional
 
 //boolean variable
-var oldEnough = false;
+var oldEnough = true;
 
 //if child is old enough, print to consol he can ride coster
 
@@ -18,3 +18,18 @@ if(oldEnough){
     //inside here will run if condition is true
     console.log("you can ride the coaster")
 }
+
+console.log("what comes after");
+
+//relational expressions
+
+//if kid is over 48 inches he can ride
+
+var kidHeight = 50;
+var minHeight = 48;
+
+if(kidHeight<minHeight){
+    console.log("you can ride")
+
+}
+
