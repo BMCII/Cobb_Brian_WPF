eec2749e650f4a3bb8b41bd80e55f8446f64fe7b
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-16 12:15:25 -0400

    array
    started variables and created an array

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,9 +5,15 @@
 Expressions Wacky
  */
 
-//
+//possible snack combinations
 
+var ingredient1 = prompt("what is your favorite snack food?");
+var ingredient2 = prompt("what is your second favorite snack food?");
+var ingredient3 = prompt("what is your third favorite snack food?");
+var ingredient4 = prompt("Why not give me one more snack food.");
 
-var
+//now make array
+
+var ingredients = [ingredient1, ingredient2, ingredient3, ingredient4],;
 
 //alert("testing 1,2,3");
