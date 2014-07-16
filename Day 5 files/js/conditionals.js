42d8d47b3c1287bc8b59522e2104e2d2a8691bed
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-16 18:45:47 -0400

    starting conditionals
    starting conditionals

---
+++ Day 5 files/js/conditionals.js
@@ -0,0 +1,20 @@
+/*
+ Brian Cobb
+ WPF 1407
+ 7/14/2014
+ conditionals
+ */
+
+//alert("testing 1,2,3");
+
+//basic conditional
+
+//boolean variable
+var oldEnough = false;
+
+//if child is old enough, print to consol he can ride coster
+
+if(oldEnough){
+    //inside here will run if condition is true
+    console.log("you can ride the coaster")
+}
