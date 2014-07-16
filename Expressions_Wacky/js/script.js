7a7a62af77404d5d1e5e2919cd3348ba6d6232a9
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-16 12:54:29 -0400

    started different direction
    committing before I change too much

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -7,13 +7,22 @@ Expressions Wacky
 
 //possible snack combinations
 
-var ingredient1 = prompt("what is your favorite snack food?");
-var ingredient2 = prompt("what is your second favorite snack food?");
-var ingredient3 = prompt("what is your third favorite snack food?");
+var ingredient1 = prompt("what is your favorite snack food? chips or cookies");
+var ingredient2 = prompt("what is your second favorite snack food? cookies or cake");
+var ingredient3 = prompt("what is your third favorite snack food?" + ingredient1 + " or" + ingredient2);
 var ingredient4 = prompt("Why not give me one more snack food.");
+
+console.log(ingredient3);
 
 //now make array
 
-var ingredients = [ingredient1, ingredient2, ingredient3, ingredient4],;
+var ingredients = [ingredient1, ingredient2, ingredient3];
+
+console.log(ingredients);
+var answer = alert(ingredients[0] + ingredients[1]);
+
+
+//
+
 
 //alert("testing 1,2,3");
