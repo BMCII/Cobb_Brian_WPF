41316d033219ca58cfd8b6bd638debbb119ee2d5
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-16 19:10:23 -0400

    else if statement
    else if statement

--- Day 5 files/js/conditionals.js
+++ Day 5 files/js/conditionals.js
@@ -14,6 +14,10 @@ var oldEnough = true;
 
 //if child is old enough, print to consol he can ride coster
 
+//if statement is good for checking 1 condition
+//if else chooses between 2 blocks of code
+//else if chooses between MULTIPLE blacks of code
+
 if(oldEnough){
     //inside here will run if condition is true
     console.log("you can ride the coaster")
@@ -25,17 +29,26 @@ console.log("what comes after");
 
 //if kid is over 48 inches he can ride
 
-var kidHeight = 47;
+var kidHeight = 50;
 var minHeight = 48;
+var sneakerLift = 2;
 
-if(kidHeight<minHeight){
+if(kidHeight>minHeight){
     console.log("you can ride")
 
+}  else if (kidHeight + sneakerLift > minHeight){
+    //this code will run if else statement is true
+    console.log("you are tll enough to ride the coaster if you cheat")
+
+} else {
+
+    //code will run if condition is false
+    console.log("go away")
 }
 
 //what if we had sneaker lifts
 
-var sneakerLift = 2;
+/*var sneakerLift = 2;
 //test if kid tall enough with lifts
 
 if(kidHeight + sneakerLift>minHeight){
@@ -43,3 +56,4 @@ if(kidHeight + sneakerLift>minHeight){
     console.log("you are tll enough to ride the coaster if you cheat")
 
 }
+*/
