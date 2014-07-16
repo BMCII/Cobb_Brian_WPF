ad7942e453501bd6422a76932bfb18966899a633
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-16 14:03:33 -0400

    created variables
    created variables

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,17 @@
+/*
+ Brian Cobb
+ WPF 1407
+ 7/14/2014
+ Expressions wacky
+ */
+
+alert("testing 1,2,3");
+
+
+//number of variations of ice cream
+//
+
+var flavors = ("how many flavors are there?");
+var scoops = ("how many scoops would you like");
+var toppings = ("how many toppings would you like?");
+
