4fe4836953dd888a82bae314920308edf82fb5b9
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-17 00:19:47 -0400

    Test
    still testing

--- Cobb_Brian_Conditionals_worksheet/js/script.js
+++ Cobb_Brian_Conditionals_worksheet/js/script.js
@@ -15,23 +15,28 @@ var tankCapacity = prompt("what is your gas tank capacity in gallons");
 
 //set text string "numbers" to numbers
 
-var fuelEconomyNumbers = Number(fuelEconomy);
+var fuelEconomyNumber = Number(fuelEconomy);
 var gaugeReadingNumber = Number(gaugeReading);
 var tankCapacityNumber = Number(tankCapacity);
+
+console.log(fuelEconomyNumber);
 
 //find miles able to drive on gas in tank
 //   (gaugeReading/100) * tankCapacity * fuelEconomy
 
-var distance = (gaugeReadingNumber / 100) * tankCapacityNumber * fuelEconomyNumbers;
+var distance = (gaugeReadingNumber / 100) * tankCapacityNumber * fuelEconomyNumber;
 
 //need gallons in tank
 
 // (gaugeReadingNumber / 100) * tankCapacityNumber
 
 var gasInTank = (gaugeReadingNumber / 100) * tankCapacityNumber;
+
 console.log(distance);
 console.log(gasInTank);
+
 var answer;
-answer = (distance>200)?"Yes, you can make it without stopping for gas":"You only have " + gasInTank +  " gallons of gas in your tank, better get gas now while you can!"
+answer = (distance>200)?"Yes, you can make it without stopping for gas":"You only have " + gasInTank +
+                        " gallons of gas in your tank, better get gas now while you can!";
 
 alert(answer);
