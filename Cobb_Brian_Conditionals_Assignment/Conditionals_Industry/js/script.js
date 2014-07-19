7dda5609316226d9e74fd0d2492eec15f4a6e475
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-19 00:16:10 -0400

    variables
    started making variables

--- Cobb_Brian_Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Cobb_Brian_Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -8,3 +8,16 @@
 //alert("testing 1,2,3");
 
 //what time do I need to set may alarm for
+
+
+var totalTimes = [];
+var shower = prompt("will you take a shower?");
+var brushTeeth = prompt("will you brush your teeth?");
+var breakfastCereal = 10;
+var breakfastPancakes = 30;
+
+if ( shower === "yes"){
+    totalTimes [0] = 20
+} else {
+    totalTimes [0] = 0
+}
