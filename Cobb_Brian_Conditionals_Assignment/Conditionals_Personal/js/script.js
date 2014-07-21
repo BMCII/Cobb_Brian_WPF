882758901d1421231a8cbfb2e807a73e04c57692
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-21 12:56:04 -0400

    complete
    complete

--- Cobb_Brian_Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Cobb_Brian_Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -8,8 +8,11 @@
 //alert("testing 1,2,3");
 
 //what time do I need to set may alarm for
+//start with 10 min just in case
 
-var timeUsed = 0;
+alert("enter time using 24 hour time where midnight is 00");
+
+var timeUsed = 10;
 var shower = prompt("will you take a shower?");
 var brushTeeth = prompt("will you brush your teeth?");
 var breakfast = prompt("will you be eating breakfast?");
@@ -58,19 +61,96 @@ var minutesCalculated = parseInt(minuteWorkStarts) / 60;
 var workTimeCalculated = parseInt(hourWorkStarts) + minutesCalculated;
 var alarmTime = workTimeCalculated - (timeUsed / 60);
 
+/*
 console.log(timeUsed);
 console.log(hourWorkStarts);
 console.log(minuteWorkStarts);
 console.log(minutesCalculated);
 console.log(workTimeCalculated);
 console.log(alarmTime);
+*/
 
 
 var hour = Math.floor(alarmTime);
 var minute = Math.round((alarmTime - hour) * 60);
 
+if (hour === 24){
+    hour = "00"
+
+} else if (hour === -1){
+    hour = 23
+
+} else if (hour === -2){
+    hour = 22
+
+} else if (hour === -3){
+    hour = 21
+
+} else if (hour === -4) {
+    hour = 20
+
+} else if (hour === -5) {
+    hour = 19
+
+} else if (hour === -6) {
+    hour = 18
+
+} else if (hour === -7) {
+    hour = 17
+
+} else if (hour === -8) {
+    hour = 16
+
+} else if (hour === -9) {
+    hour = 15
+
+} else if (hour === -10) {
+    hour = 14
+
+} else if (hour === -11) {
+    hour = 13
+
+} else if (hour === -12) {
+    hour = 12
+
+} else if (hour === -13) {
+    hour = 11
+
+} else if (hour === -14) {
+    hour = 10
+
+} else if (hour === -15) {
+    hour = 9
+
+} else if (hour === -16) {
+    hour = 8
+
+} else if (hour === -17) {
+    hour = 7
+
+} else if (hour === -18) {
+    hour = 6
+
+} else if (hour === -19) {
+    hour = 5
+
+} else if (hour === -20) {
+    hour = 4
+
+} else if (hour === -21) {
+    hour = 3
+
+} else if (hour === -22) {
+    hour = 2
+
+} else if (hour === -23) {
+    hour = 1
+
+}
 //minute = (minuteCalculated + timeUsed%60)*60 = 60 - (minutesCalculated + timeUsed%60);
 
+var answer = ("You need to set your alarm for " + hour + ":" + minute);
+alert(answer);
 
 console.log(hour);
 console.log(minute);
