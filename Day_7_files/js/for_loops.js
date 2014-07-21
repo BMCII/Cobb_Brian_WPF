5fcce00e3adbd49f6c9d0c9f11a6adfc2a0d90b2
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-21 18:05:30 -0400

    basic for loop
    watch out for infinte loops

---
+++ Day_7_files/js/for_loops.js
@@ -0,0 +1,16 @@
+/*
+ Brian Cobb
+ WPF 1407
+ 7/14/2014
+ Arrays
+ */
+
+//alert("testing 1,2,3");
+
+// basic for loop
+//for(initilization; condition; increment of change)
+//pros use i for counter variable comes from iteration
+
+for(var i = 0; i<0 ; i++){
+    console.log("i is " + i)
+}
