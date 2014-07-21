ce6c7acca0d7e36a3ce026565d35caeaae96e44e
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-21 14:11:06 -0400

    adding costs
    math good so far

--- Cobb_Brian_Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Cobb_Brian_Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -12,5 +12,19 @@
 
 //alert("testing 1,2,3");
 
-var basePrice = prompt("Is client website basic, ecomerce or a blog");
+var basePrice = prompt("Is client website basic, ecommerce or a blog");
+if (basePrice === "basic" || basePrice === "Basic"){
+    basePrice = 500
+
+} else if (basePrice === "ecommerce" || basePrice === "Ecommerce"){
+    var products = prompt("How many products does the client have");
+    (products>50)? basePrice = (products - 50)*10 + 1000 : basePrice = 1000;
+
+
+} else {
+    basePrice = 300
+
+}
+
+console.log(basePrice);
 
