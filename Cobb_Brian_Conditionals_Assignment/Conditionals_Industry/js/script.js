32b00de82ae843b0fe3adc01206de5021ce1c50f
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-21 10:09:26 -0400

    time
    trying to change time into decimal

--- Cobb_Brian_Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Cobb_Brian_Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -2,44 +2,7 @@
  Brian Cobb
  WPF 1407
  7/14/2014
- Conditionals Industry
+ Conditional Personal
  */
 
-//alert("testing 1,2,3");
-
-//what time do I need to set may alarm for
-
-var timeUsed = 0;
-var totalTimes = [];
-var shower = prompt("will you take a shower?");
-var brushTeeth = prompt("will you brush your teeth?");
-var breakfast = prompt("will you be eating breakfast?");
-
-
-
-
-if ( shower === "yes"){
-    timeUsed += 20
-
-}
-if (brushTeeth === "yes"){
-    timeUsed += 5
-
-}
-if (breakfast === "yes"){
-    breakfastMeal = prompt("cereal or pancakes");
-
-    if (breakfastMeal === "cereal" || "Cereal"){
-        timeUsed += 10
-
-    } else if (breakfastMeal ==="pancakes" || "Pancakes" ) {
-        timeUsed += 30
-
-    } else {
-        timeUsed += 20
-    }
-}
-var drive = prompt("how many minutes does it take you to get to work?");
-var gas = prompt("Will you need to stop for gas?");
-console.log(timeUsed);
-
+alert("testing 1,2,3");
--- Cobb_Brian_Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Cobb_Brian_Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -2,7 +2,54 @@
  Brian Cobb
  WPF 1407
  7/14/2014
- Conditional Personal
+ Conditionals Industry
  */
 
-alert("testing 1,2,3");
+//alert("testing 1,2,3");
+
+//what time do I need to set may alarm for
+
+var timeUsed = 0;
+var shower = prompt("will you take a shower?");
+var brushTeeth = prompt("will you brush your teeth?");
+var breakfast = prompt("will you be eating breakfast?");
+
+
+
+
+if ( shower === "yes"){
+    timeUsed += 20
+
+}
+if (brushTeeth === "yes"){
+    timeUsed += 5
+
+}
+if (breakfast === "yes"){
+    breakfastMeal = prompt("cereal or pancakes");
+
+    if (breakfastMeal === "cereal" || "Cereal"){
+        timeUsed += 10
+
+    } else if (breakfastMeal ==="pancakes" || "Pancakes" ) {
+        timeUsed += 30
+
+    } else {
+        timeUsed += 20
+    }
+}
+var drive = prompt("how many minutes does it take you to get to work?");
+var gas = prompt("Will you need to stop for gas?");
+
+timeUsed += drive;
+
+if (gas === "yes" || "Yes"){
+    timeUsed += 10
+}
+
+var hourWorkStarts = prompt("What is the hour your work starts?");
+var minuteWorkStarts = prompt ("at what minute does your work start?");
+
+
+console.log(timeUsed);
+
