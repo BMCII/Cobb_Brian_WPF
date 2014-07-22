deef23364eff5d4d208c8ae68138972e5a270ed0
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-21 23:04:38 -0400

    parseInt
    changed carSize to a number

--- Cobb_Brian_Conditionals_Assignment/Conditionals_Wacky/js/script.js
+++ Cobb_Brian_Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -19,13 +19,21 @@ if(clownSize === "mix" || clownSize === "Mix") {
     clownSizeNumber = 0.75
 }
 
-var carSize = prompt("How many passengers does your car normally hold");
+//clowns with blue wigs take up no room in cars
 
+var blueWigClowns = prompt("do more than two clowns have blue wigs?");
+
+
+var carSize = prompt("How many passengers does your car normally hold");
+carSize = parseInt(carSize);
 //we all know clowns can fit 1 and a half times more into a car
 console.log(clownSizeNumber);
 
 
 var answer = carSize * 1.5 / clownSizeNumber;
+if(blueWigClowns === "yes" || "Yes"){
+    answer += 3
+}
 console.log(answer);
 
 prompt("your car can fit " + answer +" of these clowns in your car.");
