38f5d76c395c5aee8170a6d4127c561fe42bddcb
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-23 12:38:23 -0400

    got it
    got it

--- Cobb_Brian_Conditionals_Assignment/Loops_worksheet/js/do_while_loop.js
+++ Cobb_Brian_Conditionals_Assignment/Loops_worksheet/js/do_while_loop.js
@@ -5,4 +5,18 @@
  Arrays
  */
 
-alert("testing 1,2,3");
+//alert("testing 1,2,3");
+
+
+var i = prompt("where do we start the countdown");
+
+do {
+    alert( i);
+    i--;
+    if(i === 0){
+        alert("blast off")
+
+    }
+
+
+} while(i>0);
