5c1e98e9c701185a4169bb4dc768ab0560a351ff
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-23 19:00:11 -0400

    basic function
    no arguments or perimeters

--- Day_8_files/js/arguments.js
+++ Day_8_files/js/arguments.js
@@ -2,7 +2,23 @@
  Brian Cobb
  WPF 1407
  7/14/2014
- Arrays
+ Argument
  */
 
 alert("testing 1,2,3");
+
+//arguments -> in function call -> goes into function
+//Paremeters -> in function definition -> catch incomeing arguments
+
+//function calc area rectangle
+
+function calcArea(){
+
+    //create var for height , width , and area
+
+    var width = 20;
+    var height = 10;
+    var are = height * width
+}
+
+calcArea();
