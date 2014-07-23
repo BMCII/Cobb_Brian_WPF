6923d2b1166eff6784bd61abdc76a7031be896a7
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-23 19:12:09 -0400

    arguments and perimeters
    working

--- Day_8_files/js/arguments.js
+++ Day_8_files/js/arguments.js
@@ -5,20 +5,30 @@
  Argument
  */
 
-alert("testing 1,2,3");
+//alert("testing 1,2,3");
 
 //arguments -> in function call -> goes into function
 //Paremeters -> in function definition -> catch incomeing arguments
 
 //function calc area rectangle
 
-function calcArea(){
+//w and h are parameters same as declaring variables and are scoped to function
+
+function calcArea(w , h){
 
     //create var for height , width , and area
 
-    var width = 20;
-    var height = 10;
-    var are = height * width
+    //var width = 20;
+    //var height = 10;
+    //var are = height * width
+
+    //create var area using parimeters
+    var area = w*h;
+    console.log(area)
 }
 
-calcArea();
+calcArea(20,10);
+calcArea(5,6);
+
+//add arguments and parameters
+//make function more flexible
