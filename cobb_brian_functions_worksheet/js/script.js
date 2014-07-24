7e3d5885a95d43c637eea0cc28a8ad83d4f318e0
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-23 22:32:59 -0400

    getting started
    getting started

--- cobb_brian_functions_worksheet/js/script.js
+++ cobb_brian_functions_worksheet/js/script.js
@@ -6,3 +6,11 @@
  */
 
 //alert("testing 1,2,3");
+
+//It takes 8.666666667 bee stings per pound to kill an animal.
+//Calculate how many bee stings are needed to kill an animal in a function
+
+var victimWeight = prompt("How much does the animal weigh?");
+function stingsToKill(){
+    
+}
