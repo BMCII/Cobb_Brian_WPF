391c0100621fb18b2e03ff8d3627b3837531db46
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-24 00:04:47 -0400

    almost done
    almost done

--- cobb_brian_functions_worksheet/js/script.js
+++ cobb_brian_functions_worksheet/js/script.js
@@ -11,26 +11,49 @@
 //Calculate how many bee stings are needed to kill an animal in a function
 
 
+
+
+
 function stingsToKill(victimWeight){
     var death = victimWeight * 8.666666667;
     return death;
 
 }
 
-var victim1 = prompt("How much does the animal weigh?");
+
+
+var victim1 = parseInt(prompt("How much does the animal weigh?"));
+
+
+while(victim1 === "" || isNaN(victim1)) {
+
+    victim1 = parseInt( prompt("Enter only numbers"));
+}
+
 var totalDeath = stingsToKill(victim1);
 
+console.log(victim1 + 10);
 alert("It takes " + totalDeath.toFixed(2) + " bee stings to kill this animal.");
 
 
 //Circumference of circle
 
 
+
 function circCirc(rad){
-    var circle = 2 * Math.PI * rad
+    var circle = 2 * Math.PI * rad;
     return circle
 }
 
-var radius = prompt("what is the radius?");
+var radius = parseInt(prompt("what is the radius?"));
 var answer = circCirc(radius);
+
+
+while(radius === "" || isNaN(radius)) {
+
+    radius = parseInt(prompt("Enter only numbers"));
+}
+alert("The circumference of the circle is " + answer.toFixed(2));
+
+
 
