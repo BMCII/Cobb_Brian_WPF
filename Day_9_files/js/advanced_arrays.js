72aecee0dddb1782e15a1aa3770fded6bfb4bc9d
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-25 19:50:11 -0400

    anon function
    anon function

--- Day_9_files/js/advanced_arrays.js
+++ Day_9_files/js/advanced_arrays.js
@@ -5,4 +5,34 @@
  Arrays
  */
 
-alert("testing 1,2,3");
+//alert("testing 1,2,3");
+
+var fruit = ["banana","pear","peach","tomato"];
+
+
+//length
+
+console.log(fruit.length);
+
+console.log(fruit);
+
+//add to end of array
+
+fruit[4] = ("cherry");
+console.log(fruit);
+fruit[fruit.length] = ("apple");
+console.log(fruit);
+
+//common so function called push
+//push() adds to next open spot in array
+
+fruit.push("mango");
+console.log(fruit);
+
+//to remove LAST item from array use pop()
+//you can catch remove item
+
+var removedFruit = fruit.pop();
+console.log(fruit);
+
+console.log(removedFruit);
