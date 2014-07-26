313454cd0c503488a3d61d02ff47f0c09e7c9950
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-26 00:26:04 -0400

    formula worked
    formula worked

--- functions_assignment/functions_personal/js/script.js
+++ functions_assignment/functions_personal/js/script.js
@@ -5,20 +5,23 @@
  Arrays
  */
 
-alert("testing 1,2,3");
+//alert("testing 1,2,3");
 
 //finding the volume of your cup or
 //volume of a truncated cone
 
+var num = (Math.PI/3);
+console.log(num);
+
 function cupVolume(h , bigR , lilR){
-    var volCup = (Math.PI/3)(h)(bigR^2+(lilR*bigR)+ lilR);
+    var volCup = num * h * (bigR^2+(lilR*bigR)+ lilR);
     return volCup
 
 }
 
-var height = prompt("how tall is your cup");
-var bigDia = prompt("how wide is the mouth of your cup");
-var lilDia = prompt("how wide is the base of your cup");
+var height = parseInt(prompt("how tall is your cup"));
+var bigDia = parseInt(prompt("how wide is the mouth of your cup"));
+var lilDia = parseInt(prompt("how wide is the base of your cup"));
 var bigRad = bigDia/2;
 var lilRad = lilDia/2;
 
