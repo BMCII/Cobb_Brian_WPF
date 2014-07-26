31e85d4564adc207cdb94a002743336447f20a5f
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-25 23:33:08 -0400

    files created
    files created

---
+++ functions_assignment/functions_personal/js/script.js
@@ -0,0 +1,20 @@
+/*
+ Brian Cobb
+ WPF 1407
+ 7/14/2014
+ Arrays
+ */
+
+alert("testing 1,2,3");
+
+//finding the volume of your cup or
+//volume of a truncated cone
+
+function cupVolume(h , bigD , lilD){
+    var cupVol = (Math.PI/3)(h)(bigD^2+(lilD*bigD)+ lilD)
+    return cupVol
+
+}
+
+var 
+
