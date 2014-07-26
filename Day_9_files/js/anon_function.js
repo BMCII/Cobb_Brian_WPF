8c000be30c3b853f4bddcc77e74b0615b8d42f48
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-25 22:01:02 -0400

    end of class
    end of class

--- Day_9_files/js/anon_function.js
+++ Day_9_files/js/anon_function.js
@@ -5,4 +5,42 @@
  Arrays
  */
 
-alert("testing 1,2,3");
+//alert("testing 1,2,3");
+
+var areaResults2 = triangle(4,5);
+console.log(areaResults2);
+
+//anon function
+
+var anonResults2 = triangle(2,9);
+console.log(anonResults2);
+
+//NO GO
+
+//anonymous functions
+//very popular in jQuery and js
+//object oriented programing
+//both styles of functions work
+//coding style
+//quick and easy
+
+//create anon function
+
+var triAreaAnon = function(b,h){
+    var area = .5 *b*h;
+    return area;
+}
+
+//create a function for a triangle
+
+//normal
+function triangle(b,h){
+    var area = .5 *b*h;
+    return area;
+}
+
+var areaResults = triangle(6,7);
+console.log(areaResults);
+
+var anonResults = triAreaAnon(3,8);
+console.log(anonResults);
