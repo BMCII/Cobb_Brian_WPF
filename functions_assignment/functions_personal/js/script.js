d2afe59167d67bc313323aa2aa524f234ff897d5
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-28 11:14:21 -0400

    notes
    Notes

--- functions_assignment/functions_personal/js/script.js
+++ functions_assignment/functions_personal/js/script.js
@@ -19,13 +19,22 @@ function cupVolume(h , bigR , lilR){
 
 }
 
-//now create variables
+//now prompt user to create variables
 
 var height = parseInt(prompt("how tall is your cup"));
+
+//validate
 var bigDia = parseInt(prompt("how wide is the mouth of your cup"));
+
+//validate
 var lilDia = parseInt(prompt("how wide is the base of your cup"));
+
+//now convert diameter to radius
+
 var bigRad = bigDia/2;
 var lilRad = lilDia/2;
+
+//create final variable and alert user
 
 var finalVolume = cupVolume(height, bigRad, lilRad);
 alert("the volume of your cup is " + finalVolume);
