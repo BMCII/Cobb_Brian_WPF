47454de20b2775e465af1c4375d799876facdef8
BMCII <briancobb@Brians-MacBook-Pro.local>
2014-07-30 13:32:05 -0400

    variables
    creating variables

--- Cobb_Brian_functions_assignment/Functions_Wacky/js/script.js
+++ Cobb_Brian_functions_assignment/Functions_Wacky/js/script.js
@@ -5,4 +5,24 @@
  Arrays
  */
 
-alert("testing 1,2,3");
+//alert("testing 1,2,3");
+
+//how many times can you ride a roller coaster before getting sick
+//create variables
+
+var topSpeed = prompt("what is the top speed roller coaster in MPH?");
+//validate
+    while(topSpeed === "" || isNaN(topSpeed)){
+    topSpeed = prompt("speed must be a number")
+    }
+
+var height = prompt ("how high is the roller coaster?");
+//validate
+    while(topSpeed === "" || isNaN(topSpeed)){
+    topSpeed = prompt("height must be a number")
+    }
+
+var eyeColor = prompt("what color are your eyes");
+
+var headAches = prompt("are you prone to headaches");
+
